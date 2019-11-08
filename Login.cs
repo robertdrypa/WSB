d2423508ae5d4e8fbc0cc30e4902ce2b946b1614
robertdrypa <robertdrypa@gmail.com>
2019-11-08 23:34:56 +0100

    Added login Function

---
+++ Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSB
+{
+	class Login
+	{
+
+		public static void checkLogin()
+		{
+			Console.WriteLine("Podaj login:");
+			string login = Console.ReadLine();
+			Console.WriteLine("Podaj haslo:");
+			string haslo = Console.ReadLine();
+
+		}
+	}
+}
